dfe248dace0d2af26b941aa7a6c5d30066f5d92b
rishavjadon456 <42317334+rishavjadon456@users.noreply.github.com>
2020-12-30 21:29:39 +0530

    changed main grid size to 2

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -15,7 +15,7 @@ function Home() {
   } = useQuery(FETCH_POSTS_QUERY);
 
   return (
-    <Grid columns={3}>
+    <Grid columns={2}>
       <Grid.Row className="page-title">
         <h1>Recent Posts</h1>
       </Grid.Row>
