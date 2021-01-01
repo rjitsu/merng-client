329a962edd3150b916739e095769450316ebbc55
rishavjadon456 <42317334+rishavjadon456@users.noreply.github.com>
2021-01-01 18:21:39 +0530

    homepage additions

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -17,6 +17,7 @@ function Home() {
   return (
     <Grid centered columns={1}>
       <Grid.Row className="page-title">
+        <h1>☄️ Qlapp, a social media platform</h1>
         <h1>Recent Posts</h1>
       </Grid.Row>
       <Grid.Row>
